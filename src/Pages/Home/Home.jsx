bd5c39a137f2ade665cded2d9978d471814d49fd
kurra chandu <nanic6465@gmail.com>
2023-08-22 23:42:46 +0530

    message

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import Chat from './Chat'
+import React from 'react';
+import Chat from '../../Components/Chat'; // Update the path based on your project structure
+import Sidebar from '../../Components/Sidebar'; // Update the path based on your project structure
+import '../../../src/style.scss'; // Update the path for your CSS file
+// import Search from '../../Components/Search';
+
 function Home() {
   return (
-    <div>
-      <Chat/>
+    <div className='home'>
+      <div className='container'>
+         <Sidebar />
+        
+        <Chat/>
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
